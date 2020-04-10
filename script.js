11bfa1b21252c5be6b8aaa6bef7fcfa5dcd6e818
RJC Desktop <reuben.crocker@gmail.com>
2020-04-10 17:57:44 +0800

    fixed error in code (special -> symbol)

--- script.js
+++ script.js
@@ -11,60 +11,98 @@ const randomFunc = {
   lower: getRandomLower,
   upper: getRandomUpper,
   number: getRandomDigit,
-  special: getRandomSpecial
+  symbols: getRandomSpecial
 };
 
 // Add event listener to generate button
-generate.addEventListener('click', () => {
+generateEl.addEventListener('click', () => {
   const length = Number(lengthEl.value);
   const hasLower = lowercaseEl.checked;
   const hasUpper = uppercaseEl.checked;
   const hasDigit = digitsEl.checked;
   const hasSpecial = specialEl.checked;
 
-  resultEl.innerText = generatePassword(hasLower, hasUpper, hasDigit, hasSpecial);
+  let pass = generatePassword(hasLower, hasUpper, hasDigit, hasSpecial, length);
+  resultEl.innerText = pass.substring(0, length)
 });
-
-var property2 = true;
-{
-  property: 'value',
-  property2,
-}
 
 //generate password function
 function generatePassword(lower, upper, number, symbols, length) {
   //1. Init pw var
   //2. filter out unchecked types
   //3. loop over length call generator function for each type
-  //4. add findal pw to the pw var and return
+  //4. add final pw to the pw var and return
 
-  let generatedPassword = '';
+  let generatedPassword = "";
 
   const typesCount = lower + upper + number + symbols
 
   console.log("typesCount: ", typesCount);
 
-  const typesArr = [{lower}, {upper}, {number}, {symbols}].filter(item => Object.values(item));
+  const typesArr = [{lower}, {upper}, {number}, {symbols}].filter(
+    item => Object.values(item)[0]
+  );
 
   console.log('typesArr: ', typesArr);
+
+  if(typesCount === 0){
+    return '';
+  }
+
+  for(let i = 0; i<length; i+=typesCount){
+    typesArr.forEach(type => {
+      const funcName = Object.keys(type)[0];
+      generatedPassword += randomFunc[funcName]();
+
+      // console.log("funcName: ", funcName)
+      // console.log("randomFunc[funcname]", randomFunc[funcName])
+    });
+  }
+
+  return generatedPassword;
 }
 
 
 // Generator Functions
 
-function getRandomLower(){
-  return String.fromCharCode(Math.floor(Math.random()*26) + 97);
-}
+// function getRandomLower(){
+//   return String.fromCharCode(Math.floor(Math.random()*26) + 97);
+// }
 
-function getRandomUpper(){
-  return String.fromCharCode(Math.floor(Math.random()*26) + 65);
-}
+// function getRandomUpper(){
+//   return String.fromCharCode(Math.floor(Math.random()*26) + 65);
+// }
 
-function getRandomDigit(){
-  return String.fromCharCode(Math.floor(Math.random()*10) + 48);
-}
+// function getRandomDigit(){
+//   return String.fromCharCode(Math.floor(Math.random()*10) + 48);
+// }
 
-function getRandomSpecial(){
-  const symbols = '`~!@#$%^&*()_-+={[}]\\|:;"<,>.?/\''
-  return symbols[Math.floor(Math.random()*symbols.length)];
-}
+// function getRandomSpecial(){
+//   const symbols = [
+//     '@',
+//     '%',
+//     '+',
+//     '\\',
+//     '/',
+//     "'",
+//     '!',
+//     '#',
+//     '$',
+//     '^',
+//     '?',
+//     ':',
+//     ',',
+//     ')',
+//     '(',
+//     '}',
+//     '{',
+//     ']',
+//     '[',
+//     '~',
+//     '-',
+//     '_',
+//     '.'
+//   ]
+//   return symbols[Math.floor(Math.random()*symbols.length)];
+// }
+
